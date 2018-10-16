0e019d1727511ddd28028a2608bc53c3bc8a3862
brucremo <brualexander@gmail.com>
2018-10-15 20:45:55 -0400

    age added

--- db_apis/users.js
+++ db_apis/users.js
@@ -28,6 +28,7 @@ const createSql =
   USERID,
   USERPASSWORD,
   USERNAME,
+  USERAGE,
   USERSUMMARY,
   USERJOINDATE,
   USERAVATAR,
@@ -36,6 +37,7 @@ const createSql =
     :USERID,
     :USERPASSWORD,
     :USERNAME,
+    :USERAGE,
     :USERSUMMARY,
     :USERJOINDATE,
     :USERAVATAR,
@@ -56,36 +58,11 @@ async function create(usr) {
 module.exports.create = create;
 
 //Update
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
 const updateSql =
  `update USERS
   set USERPASSWORD = :USERPASSWORD,
   USERNAME = :USERNAME,
+  USERAGE = :USERAGE,
   USERSUMMARY = :USERSUMMARY,
   USERJOINDATE = :USERJOINDATE,
   USERAVATAR = :USERAVATAR,
