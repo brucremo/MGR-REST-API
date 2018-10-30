2b0390a2f8790631934b2be8310c3cd7ec42fdf6
brucremo <brualexander@gmail.com>
2018-10-30 15:15:58 -0400

    Fix for RESETCODE size

--- db_apis/reset.js
+++ db_apis/reset.js
@@ -22,7 +22,7 @@ async function process(usr, qry) {
         RESETCODE : bcrypt.hashSync(usr.USERID + qry.USEREMAIL + Math.random())
     };
 
-    param.RESETCODE = param.RESETCODE.substr(0, 8);
+    param.RESETCODE = param.RESETCODE.substr(8, param.RESETCODE.length);
 
     await database.Query("update USERS set RESETCODE = :RESETCODE where USERID = :USERID", param);
     //Create trigger to get the current date
