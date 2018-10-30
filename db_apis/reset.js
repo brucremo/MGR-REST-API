c307fcd956b54ef6deaeb02380f0741329189e7e
brucremo <brualexander@gmail.com>
2018-10-30 14:54:21 -0400

    replace $ with .

--- db_apis/reset.js
+++ db_apis/reset.js
@@ -23,6 +23,7 @@ async function process(usr, qry) {
     };
 
     param.RESETCODE = param.RESETCODE.replace('/','.');
+    param.RESETCODE = param.RESETCODE.replace('$','.');
 
     await database.Query("update USERS set RESETCODE = :RESETCODE where USERID = :USERID", param);
     //Create trigger to get the current date
