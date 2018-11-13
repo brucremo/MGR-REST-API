bc02dc2c9473a8b59a7dda06265420a5971b8285
brucremo <brualexander@gmail.com>
2018-11-12 21:45:03 -0500

    Bad result filtering

--- controllers/igdb.js
+++ controllers/igdb.js
@@ -5,16 +5,31 @@ const client = igdb('aef56bac5ab539faba6d9f9b9429487b');
 async function getByName(req, res, next) {
     try {
 
-        if(req.params.offset == undefined){
+        if (req.params.offset == undefined) {
             req.params.offset = 0;
         }
-        
+
         client.games({
-            fields: '*', // Return all fields
-            limit: 10, // Limit results
+            fields: [
+                'name', 'cover', 'summary', 'screenshots', 'aggregated_rating'
+            ], // Return all fields
+            limit: 20, // Limit results
             offset: req.params.offset,
             search: req.params.name
         }).then(response => {
+
+            for (var i = 0; i < response.body.length; i++) {
+
+                if (response.body[i].cover != undefined && response.body[i].screenshots != undefined) {
+                    //Getting a larger size image, instead of a thumbnail following IGDB documentation
+                    response.body[i].cover.url = response.body[i].cover.url.replace("thumb", "1080p");
+                } else {
+
+                    response.body.splice(i, 1);
+                    i -= 1;
+                }
+            }
+
             res.status(200).json(response.body);
         }).catch(err => {
             res.status(429).end(err.message);
@@ -36,7 +51,9 @@ async function getGame(req, res, next) {
             ids: [
                 req.params.id
             ],
-            fields: '*', // Return all fields
+            fields: [
+                'name', 'cover', 'summary', 'screenshots', 'aggregated_rating'
+            ] // Return all fields
 
         }).then(response => {
             //res.status(200).json(response.body);
@@ -49,10 +66,21 @@ async function getGame(req, res, next) {
                 fields: 'name'
             }).then(response => {
                 game[0].developers = response.body;
+
+                for (var i = 0; i < game[0].screenshots.length; i++) {
+                    game[0].screenshots[i].url = game[0].screenshots[i].url.replace("thumb", "1080p");
+                }
+
+                game[0].aggregated_rating = Math.round(game[0].aggregated_rating);
+
+                game[0].cover.url = game[0].cover.url.replace("thumb", "1080p");
+
                 res.status(200).json(game[0]);
             }).catch(err => {
                 res.status(429).end(err.message);
             });
+
+
 
         }).catch(err => {
             res.status(429).end(err.message);
